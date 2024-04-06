40dc46b51902d8afbcca420a2cc95635b85d2bfb
G00398070 <G00398070@atu.ie>
2024-04-06 10:08:33 +0100

    getting data from mongodb
    Signed-off-by: G00398070 <G00398070@atu.ie>

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
-import { Dashboard } from "@/components/Dashboard";
-import Image from "next/image";
+import { Dashboard } from '@/components/Dashboard'
 
-export default function Home() {
-  return (
-    <Dashboard/>
-  );
+export default async function Home() {
+  const res = await fetch(`http://localhost:3000/getAll`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    //next.js middleware that only revalidates data every 5 seconds
+    next: {
+      revalidate: 5,
+    },
+  })
+
+  const body = await res.json()
+  console.log('response in page.js:' + JSON.stringify(body))
+  return <Dashboard />
 }
